422cf8e281eccbf36a899927e16e43ba28325a7f
Valera Bronshtein <105710429+valeraBr@users.noreply.github.com>
2022-10-27 11:15:14 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #Benchmarks
 
-./memphis-benchmarks opType=produce msgSize=256 msgCount=100000 host=memphis-cluster.memphis.svc.cluster.local username=root token=$TOKEN storageType=file replicas=1 cg=cg1 pullInterval=100 batchSize=20 batchTTW=40 concurrency=150 iterations=10 stationsCount=1 printHeader=false >> 256.csv
-./memphis-benchmarks opType=produce msgSize=512 msgCount=100000 host=memphis-cluster.memphis.svc.cluster.local username=root token=$TOKEN storageType=file replicas=1 cg=cg1 pullInterval=100 batchSize=20 batchTTW=40 concurrency=150 iterations=10 stationsCount=1 printHeader=false >> 512.csv
-./memphis-benchmarks opType=produce msgSize=1024 msgCount=100000 host=memphis-cluster.memphis.svc.cluster.local username=root token=$TOKEN storageType=file replicas=1 cg=cg1 pullInterval=100 batchSize=20 batchTTW=40 concurrency=150 iterations=10 stationsCount=1 printHeader=false >> 1024.csv
-./memphis-benchmarks opType=produce msgSize=5120 msgCount=100000 host=memphis-cluster.memphis.svc.cluster.local username=root token=$TOKEN storageType=file replicas=1 cg=cg1 pullInterval=100 batchSize=20 batchTTW=40 concurrency=150 iterations=10 stationsCount=1 printHeader=false >> 5120.csv
+./memphis-benchmarks opType=produce msgSize=256 msgCount=100000 host=memphis-cluster.memphis.svc.cluster.local username=root token=$TOKEN storageType=file replicas=1 cg=cg1 pullInterval=100 batchSize=20 batchTTW=40 concurrency=150 iterations=10 stationsCount=1 printHeader=true asyncProduce=false
+./memphis-benchmarks opType=produce msgSize=512 msgCount=100000 host=memphis-cluster.memphis.svc.cluster.local username=root token=$TOKEN storageType=file replicas=1 cg=cg1 pullInterval=100 batchSize=20 batchTTW=40 concurrency=150 iterations=10 stationsCount=1 printHeader=false asyncProduce=false
+./memphis-benchmarks opType=produce msgSize=1024 msgCount=100000 host=memphis-cluster.memphis.svc.cluster.local username=root token=$TOKEN storageType=file replicas=1 cg=cg1 pullInterval=100 batchSize=20 batchTTW=40 concurrency=150 iterations=10 stationsCount=1 printHeader=false asyncProduce=false
+./memphis-benchmarks opType=produce msgSize=5120 msgCount=100000 host=memphis-cluster.memphis.svc.cluster.local username=root token=$TOKEN storageType=file replicas=1 cg=cg1 pullInterval=100 batchSize=20 batchTTW=40 concurrency=150 iterations=10 stationsCount=1 printHeader=false asyncProduce=false
